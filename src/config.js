51d73e1bbdf4f7c760080000720a873018cf2b26
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-06-02 20:44:35 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "MTIwODQ1OTIyMzYwNDY2MjMxMg.GIq3dK.nE7BUXGbJU-uks3bEdo3mvIn1jb4YfEZeRc3rA",
-    developerServer: "1231666773439545465",
+    token: "",
+    developerServer: "",
 
     bot: {
         komutları_sil: false,
