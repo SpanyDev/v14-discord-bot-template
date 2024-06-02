cc0728abc1c7e5a1bb5fc5a3acbb09e9ef108844
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-06-02 20:45:20 +0300

    Update test.js

--- src/Commands/User/test.js
+++ src/Commands/User/test.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('asdasd')
+        .setName('ping')
         .setDescription('Replies with pong!'),
     run: async (client, interaction) => {
         await interaction.reply({
