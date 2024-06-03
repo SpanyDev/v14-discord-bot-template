2a10391e5306e0069f45b194866ce09d09f84acf
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-06-03 14:16:11 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,13 @@ module.exports = {
     token: "",
     developerServer: "",
 
+    botCustom: {
+        logo: "",
+        color: "#9733ef",
+        redColor: "#ef3333",
+        greenColor: "#33ef5a",
+    },
+
     bot: {
         komutları_sil: false,
         global_komutlar: false,
